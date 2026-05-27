ba9a6840768db505f2a097826cbe513ae54ba6cb
Geovany <geovanynunes7007@gmail.com>
2026-05-27 20:22:58 -0300

    backend criado

---
+++ backend/apiruasegura/src/main/java/com/meuprojeto/apiruasegura/ApiRuaSeguraApplication.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.apiruasegura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRuaSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRuaSeguraApplication.class, args);
+	}
+
+}
---
+++ backend/apiruasegura/src/test/java/com/meuprojeto/apiruasegura/ApiruaseguraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.apiruasegura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiruaseguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
